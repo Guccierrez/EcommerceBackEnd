348c8cba7a393fecc2679df463e48e5411885482
Fabian Gutierrez <fabiangutierrez580@gmail.com>
2022-10-06 17:45:26 -0500

    got all seeds inserted

--- models/Product.js
+++ models/Product.js
@@ -28,11 +28,18 @@ Product.init(
     },
     stock: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      defaultValue:10,
+      Validate: {
+        isNumeric: true 
+      }
     },
     category_id: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      references: {
+        model: 'category',
+        key: 'id'
+      }
 
     }
 
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,19 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({
+      include: [
+        { model: Product,},
+      ],
+    });
+    res.json(categories);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.get('/:id', (req, res) => {
