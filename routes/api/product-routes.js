de643089b102dadcbd35708364b96c456485b32f
Fabian Gutierrez <fabiangutierrez580@gmail.com>
2022-10-11 18:03:37 -0500

    added photo and a video of walkthrough

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,16 +4,34 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      include: [{ model: Category,Tag,},],
+    });
+
+    res.json(products);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
-});
+  try {
+    const products = await Product.findByPk(req.params.id,{
+      include: [{ model: Category,Tag,},],
+    });
+  
+    res.json(products);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+  });
+
+
 
 // create new product
 router.post('/', (req, res) => {
@@ -78,7 +96,7 @@ router.put('/:id', (req, res) => {
 
       // run both actions
       return Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
+        ProductTag.destroy({ where: {id: productTagsToRemove } }),
         ProductTag.bulkCreate(newProductTags),
       ]);
     })
@@ -89,8 +107,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+  try {
+    const products = await Product.destroy( {
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
+
